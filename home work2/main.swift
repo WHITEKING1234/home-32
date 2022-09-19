f991cb50abd8ba14969e9fe5496609e510d99f91
bombag456 <bombag456@gmail.com>
2022-09-19 23:13:43 +0600

    dqdq

--- home work2/main.swift
+++ home work2/main.swift
@@ -10,29 +10,35 @@ import Foundation
 print("Hello, World!")
 
 
-print("Привет Меня зовут Александрия")
-print("Я могу узнать погоду в любой области в реал тайме")
-print("Проверишь мои способности?")
-let ad2=readLine()!
-print("Выбери одну из областей Бишкек-Ош-Талас-Жаллабад)")
-let sff=readLine()!
-func season(see:Int){
-    switch see{
-    case 1:print("В Бишкеке-18 градусов(возможны осадки)")
-    case 2:print("очень жарко 25 градусов")
-    case 3:print("20 градусов")
-    case 4:print("+20 градусов")
-    default:
-        print("no item found")
+var bishkek = "20 градусов"
+let osh = "30 градусов очень жарко"
+var Talas = "19 градусов"
+func gorod(gorod2:String){
+    if gorod2 == "bishkek"{
+        print(bishkek)
+    }else if gorod2 == "osh"{
+        print(osh)
+    }else if gorod2 == "Talas"{
+        print(Talas)
+    }else{
+        print("erro girl")
     }
 }
+    let gorod2 = readLine()!
+gorod(gorod2:String(gorod2))
+print("Какую область тебе еще рассказать")
 
+var Karakol = "19 градусов без осодков"
+let Jalabad = "Как и в оше Жарко"
+func gorod(gorod3:String){
+    if gorod3 == "Karakol"{
+        print(Karakol)
+    }else if gorod3 == "Jalabad"{
+        print(Jalabad)
+        }else{
+        print("erro girl")
+    }
+}
+    let gorod3 = readLine()!
+gorod(gorod3:String(gorod3))
 
-season(see: 1)
-let see=readLine()!
-season(see: 2)
-let see2=readLine()!
-season(see: 3)
-let see3=readLine()!
-season(see: 4)
-print("По моим меркам ближащию неделю ппрогнозы не поменяются")
